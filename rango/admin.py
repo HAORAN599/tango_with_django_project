d855d5889233ee19331b5859e9cd52e33e70b40b
Haoran Wei <3075353W@student.gla.ac.uk>
2026-02-06 22:39:49 +0000

    chapter5 finished

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from rango.models import Category,Page
+
+class PageAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Category)  #zhu ce moXing,ran ta houTai xianShi
+admin.site.register(Page,PageAdmin)
 
 # Register your models here.
