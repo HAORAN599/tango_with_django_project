91dcf6a76c557ddf961ad9571d8844e288f55ca2
Haoran Wei <3075353W@student.gla.ac.uk>
2026-02-09 11:16:43 +0000

    chapter6 finished

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import title
+from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
@@ -7,6 +8,12 @@ class Category(models.Model):
 
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args,**kwargs)
+
 
     class Meta:
         verbose_name_plural = 'categories'
